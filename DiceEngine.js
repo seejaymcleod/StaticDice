f58aab9ed506657ce145fe86773851761120f62c
seejaymcleod <seejaymcleod@gmail.com>
2026-05-15 09:25:43 -0400

    upgraded entropy check very much

--- DiceEngine.js
+++ DiceEngine.js
@@ -11,7 +11,32 @@ class DiceEngine {
             countThreshOp: "", countThreshVal: null,
             setsOp: "", setsVal: null
         };
-        this.savedQueues = [];
+        this.savedQueues = [
+            {
+                id: 'lots-preset',
+                name: 'Lots (DEBUG)',
+                color: '#38bdf8',
+                queue: [
+                    { count: 7, sides: 6, id: 1 },
+                    { count: 3, sides: 8, id: 2 },
+                    { count: 1, sides: 100, id: 3 }
+                ],
+                modifier: null,
+                modLevel: 1,
+                flat: 0,
+                rules: {
+                    targetMode: 'count',
+                    targetOp: '>=',
+                    targetVal: 2,
+                    rerollOp: '=',
+                    rerollVal: 1,
+                    explodeOp: '=',
+                    explodeVal: 2,
+                    setsOp: '=',
+                    setsVal: 1
+                }
+            }
+        ];
         this.rng = this.defaultRng;
         this.overallTarget = null;
     }
@@ -129,6 +154,23 @@ class DiceEngine {
         return newSaved;
     }
 
+    loadLotsTest() {
+        this.rollingQueue = [
+            { count: 7, sides: 6, id: Date.now() + 1 },
+            { count: 3, sides: 8, id: Date.now() + 2 },
+            { count: 1, sides: 100, id: Date.now() + 3 }
+        ];
+        this.activeModifier = null;
+        this.modifierLevel = 1;
+        this.flatMod = 0;
+        this.rollRules = {
+            targetMode: 'count', targetOp: '>=', targetVal: 2,
+            rerollOp: '=', rerollVal: 1,
+            explodeOp: '=', explodeVal: 2,
+            setsOp: '=', setsVal: 1
+        };
+    }
+
     loadQueue(id) {
         const item = this.savedQueues.find(q => q.id === id);
         if (!item) return;
@@ -242,7 +284,7 @@ class DiceEngine {
                 }
                 groupExplosions += explodeCount;
 
-                if (activeRules.targetOp || activeRules.targetMode === 'count') {
+                if (activeRules.targetOp || activeRules.targetMode === 'count' || activeRules.setsOp) {
                     pool.push(kept);
                     if (explodeCount > 0) {
                         let parts = dieLog.split('!->');
