4a602961777feb58605c4dcff25ef7d4b110a56c
seejaymcleod <seejaymcleod@gmail.com>
2026-05-30 21:24:28 -0400

    SD template tweaks

--- Assets/TemplatesData.js
+++ Assets/TemplatesData.js
@@ -15,13 +15,69 @@ window.StaticDiceTemplates = [
             "MaxHP": "10",
             "AC": "10",
             "STR": "10", "DEX": "10", "CON": "10", "INT": "10", "WIS": "10", "CHA": "10",
-            "STR_mod": "0", "DEX_mod": "0", "CON_mod": "0", "INT_mod": "0", "WIS_mod": "0", "CHA_mod": "0"
+            "STR_mod": "0", "DEX_mod": "0", "CON_mod": "0", "INT_mod": "0", "WIS_mod": "0", "CHA_mod": "0",
+            "Attack_Melee": "0", "Attack_Ranged": "0", "Spellcheck": "0", "Backstab_Dice_Bonus": "0"
         },
         groups: [
+            { id: 'details', name: 'Details', color: '#00ff88' },
             { id: 'stats', name: 'Stats', color: '#00d4ff' },
             { id: 'combat', name: 'Combat', color: '#ff003c' }
         ],
         widgets: [
+            {
+                id: 'w_class',
+                groupId: 'details',
+                name: '',
+                detailText: 'Class',
+                widgetType: 'text',
+                text: '',
+                collapsed: false
+            },
+            {
+                id: 'w_ancestry',
+                groupId: 'details',
+                name: '',
+                detailText: 'Ancestry',
+                widgetType: 'text',
+                text: '',
+                collapsed: false
+            },
+            {
+                id: 'w_background',
+                groupId: 'details',
+                name: '',
+                detailText: 'Background',
+                widgetType: 'text',
+                text: '',
+                collapsed: false
+            },
+            {
+                id: 'w_deity',
+                groupId: 'details',
+                name: '',
+                detailText: 'Deity',
+                widgetType: 'text',
+                text: '',
+                collapsed: false
+            },
+            {
+                id: 'w_alignment',
+                groupId: 'details',
+                name: '',
+                detailText: 'Alignment',
+                widgetType: 'text',
+                text: '',
+                collapsed: false
+            },
+            {
+                id: 'w_languages',
+                groupId: 'details',
+                name: '',
+                detailText: 'Languages',
+                widgetType: 'text',
+                text: '',
+                collapsed: false
+            },
             {
                 id: 'w_hp',
                 groupId: 'stats',
@@ -29,7 +85,8 @@ window.StaticDiceTemplates = [
                 widgetType: 'stepper',
                 min: 0,
                 max: 10,
-                value: 10
+                value: 10,
+                showTracker: true
             },
             {
                 id: 'w_ac',
@@ -182,25 +239,194 @@ window.StaticDiceTemplates = [
                 hidden: true
             },
             {
-                id: 'w_atk',
+                id: 'w_mod_str',
+                groupId: 'stats',
+                name: 'STR Modifier',
+                widgetType: 'number',
+                value: 0,
+                bindsVariable: 'STR_mod',
+                variableRelType: 'define',
+                hidden: true
+            },
+            {
+                id: 'w_mod_dex',
+                groupId: 'stats',
+                name: 'DEX Modifier',
+                widgetType: 'number',
+                value: 0,
+                bindsVariable: 'DEX_mod',
+                variableRelType: 'define',
+                hidden: true
+            },
+            {
+                id: 'w_mod_con',
+                groupId: 'stats',
+                name: 'CON Modifier',
+                widgetType: 'number',
+                value: 0,
+                bindsVariable: 'CON_mod',
+                variableRelType: 'define',
+                hidden: true
+            },
+            {
+                id: 'w_mod_int',
+                groupId: 'stats',
+                name: 'INT Modifier',
+                widgetType: 'number',
+                value: 0,
+                bindsVariable: 'INT_mod',
+                variableRelType: 'define',
+                hidden: true
+            },
+            {
+                id: 'w_mod_wis',
+                groupId: 'stats',
+                name: 'WIS Modifier',
+                widgetType: 'number',
+                value: 0,
+                bindsVariable: 'WIS_mod',
+                variableRelType: 'define',
+                hidden: true
+            },
+            {
+                id: 'w_mod_cha',
+                groupId: 'stats',
+                name: 'CHA Modifier',
+                widgetType: 'number',
+                value: 0,
+                bindsVariable: 'CHA_mod',
+                variableRelType: 'define',
+                hidden: true
+            },
+            {
+                id: 'w_var_attack_melee',
+                groupId: 'stats',
+                name: 'Attack Melee Mod',
+                widgetType: 'number',
+                value: 0,
+                bindsVariable: 'Attack_Melee',
+                variableRelType: 'define',
+                hidden: true
+            },
+            {
+                id: 'w_var_attack_ranged',
+                groupId: 'stats',
+                name: 'Attack Ranged Mod',
+                widgetType: 'number',
+                value: 0,
+                bindsVariable: 'Attack_Ranged',
+                variableRelType: 'define',
+                hidden: true
+            },
+            {
+                id: 'w_var_spellcheck',
+                groupId: 'stats',
+                name: 'Spellcheck Mod',
+                widgetType: 'number',
+                value: 0,
+                bindsVariable: 'Spellcheck',
+                variableRelType: 'define',
+                hidden: true
+            },
+            {
+                id: 'w_var_backstab_dice_bonus',
+                groupId: 'stats',
+                name: 'Backstab Dice Bonus',
+                widgetType: 'number',
+                value: 0,
+                bindsVariable: 'Backstab_Dice_Bonus',
+                variableRelType: 'define',
+                hidden: true
+            },
+            {
+                id: 'w_atk_melee_str',
                 groupId: 'combat',
-                name: 'Melee Attack Roll',
+                name: 'Attack Melee - STR',
                 widgetType: 'roller',
                 unifiedQueue: [
                     { nodeType: 'node', sides: 20, count: 1 },
                     { nodeType: 'operator', operator: '+' },
-                    { nodeType: 'modifier', type: 'variable', value: 'STR_mod', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' }
+                    { nodeType: 'modifier', type: 'variable', value: 'STR_mod', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' },
+                    { nodeType: 'operator', operator: '+' },
+                    { nodeType: 'modifier', type: 'variable', value: 'Attack_Melee', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' }
                 ]
             },
             {
-                id: 'w_dmg',
+                id: 'w_atk_melee_dex',
                 groupId: 'combat',
-                name: 'Sword Damage Roll',
+                name: 'Attack Melee - DEX',
                 widgetType: 'roller',
                 unifiedQueue: [
-                    { nodeType: 'node', sides: 8, count: 1 },
+                    { nodeType: 'node', sides: 20, count: 1 },
                     { nodeType: 'operator', operator: '+' },
-                    { nodeType: 'modifier', type: 'variable', value: 'STR_mod', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' }
+                    { nodeType: 'modifier', type: 'variable', value: 'DEX_mod', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' },
+                    { nodeType: 'operator', operator: '+' },
+                    { nodeType: 'modifier', type: 'variable', value: 'Attack_Melee', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' }
+                ]
+            },
+            {
+                id: 'w_atk_ranged_str',
+                groupId: 'combat',
+                name: 'Attack Ranged - STR',
+                widgetType: 'roller',
+                unifiedQueue: [
+                    { nodeType: 'node', sides: 20, count: 1 },
+                    { nodeType: 'operator', operator: '+' },
+                    { nodeType: 'modifier', type: 'variable', value: 'STR_mod', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' },
+                    { nodeType: 'operator', operator: '+' },
+                    { nodeType: 'modifier', type: 'variable', value: 'Attack_Ranged', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' }
+                ]
+            },
+            {
+                id: 'w_atk_ranged_dex',
+                groupId: 'combat',
+                name: 'Attack Ranged - DEX',
+                widgetType: 'roller',
+                unifiedQueue: [
+                    { nodeType: 'node', sides: 20, count: 1 },
+                    { nodeType: 'operator', operator: '+' },
+                    { nodeType: 'modifier', type: 'variable', value: 'DEX_mod', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' },
+                    { nodeType: 'operator', operator: '+' },
+                    { nodeType: 'modifier', type: 'variable', value: 'Attack_Ranged', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' }
+                ]
+            },
+            {
+                id: 'w_spellcheck_int',
+                groupId: 'combat',
+                name: 'Spellcheck - INT',
+                widgetType: 'roller',
+                unifiedQueue: [
+                    { nodeType: 'node', sides: 20, count: 1 },
+                    { nodeType: 'operator', operator: '+' },
+                    { nodeType: 'modifier', type: 'variable', value: 'INT_mod', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' },
+                    { nodeType: 'operator', operator: '+' },
+                    { nodeType: 'modifier', type: 'variable', value: 'Spellcheck', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' }
+                ]
+            },
+            {
+                id: 'w_spellcheck_wis',
+                groupId: 'combat',
+                name: 'Spellcheck - WIS',
+                widgetType: 'roller',
+                unifiedQueue: [
+                    { nodeType: 'node', sides: 20, count: 1 },
+                    { nodeType: 'operator', operator: '+' },
+                    { nodeType: 'modifier', type: 'variable', value: 'WIS_mod', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' },
+                    { nodeType: 'operator', operator: '+' },
+                    { nodeType: 'modifier', type: 'variable', value: 'Spellcheck', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' }
+                ]
+            },
+            {
+                id: 'w_spellcheck_cha',
+                groupId: 'combat',
+                name: 'Spellcheck - CHA',
+                widgetType: 'roller',
+                unifiedQueue: [
+                    { nodeType: 'node', sides: 20, count: 1 },
+                    { nodeType: 'operator', operator: '+' },
+                    { nodeType: 'modifier', type: 'variable', value: 'CHA_mod', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' },
+                    { nodeType: 'operator', operator: '+' },
+                    { nodeType: 'modifier', type: 'variable', value: 'Spellcheck', operator: '+', multiplierType: 'none', multiplierValue: 1, divisorType: 'none', divisorValue: 1, roundMode: 'none' }
                 ]
             }
         ]
