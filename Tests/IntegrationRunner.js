642276d83a77cf91054d9dfcfb58c7eae32a25fe
seejaymcleod <seejaymcleod@gmail.com>
2026-06-02 17:02:49 -0400

    parsing rules

--- Tests/IntegrationRunner.js
+++ Tests/IntegrationRunner.js
@@ -756,20 +756,20 @@ window.addEventListener('load', () => {
                 if (!hSlot9 || hSlot9.name !== '') throw new Error('Horlabo gear slot 9 should be empty');
 
                 // Verify parsed ambition talent and other merged class talents
-                const ambitionTalentW = horlaboWidgets.find(w => w.name === 'HUMAN AMBITION-1: SPELL MASTERY (DETECT MAGIC)');
+                const ambitionTalentW = horlaboWidgets.find(w => w.name === 'SPELL MASTERY (DETECT MAGIC)' && w.addonNote === 'Bonus: Human Ambition(Ancestry)-1');
                 if (!ambitionTalentW) throw new Error('Human ambition talent SPELL MASTERY should be parsed and created');
                 if (!ambitionTalentW.text || !ambitionTalentW.text.includes('advantage on casting one spell')) throw new Error('Human ambition talent description is incorrect');
 
-                const statBonusTalentW = horlaboWidgets.find(w => w.name === 'WIZARD-1: STAT BONUS (+2 INT)');
+                const statBonusTalentW = horlaboWidgets.find(w => w.name === 'STAT BONUS (+2 INT)' && w.addonNote === 'Bonus: Wizard(Class)-1');
                 if (!statBonusTalentW) throw new Error('Wizard level 1 STAT BONUS (+2 INT) widget should be created');
 
-                const statBonusTalentW5 = horlaboWidgets.find(w => w.name === 'WIZARD-5: STAT BONUS (+2 INT)');
+                const statBonusTalentW5 = horlaboWidgets.find(w => w.name === 'STAT BONUS (+2 INT)' && w.addonNote === 'Bonus: Wizard(Class)-5');
                 if (!statBonusTalentW5) throw new Error('Wizard level 5 STAT BONUS (+2 INT) widget should be created');
 
-                const castingTalentW7 = horlaboWidgets.find(w => w.name === 'WIZARD-7: SPELLCASTING (+1 CASTING)');
+                const castingTalentW7 = horlaboWidgets.find(w => w.name === 'SPELLCASTING (+1 CASTING)' && w.addonNote === 'Bonus: Wizard(Class)-7');
                 if (!castingTalentW7) throw new Error('Wizard level 7 SPELLCASTING (+1 CASTING) widget should be created');
 
-                const castingTalentW9 = horlaboWidgets.find(w => w.name === 'WIZARD-9: SPELLCASTING (+1 CASTING)');
+                const castingTalentW9 = horlaboWidgets.find(w => w.name === 'SPELLCASTING (+1 CASTING)' && w.addonNote === 'Bonus: Wizard(Class)-9');
                 if (!castingTalentW9) throw new Error('Wizard level 9 SPELLCASTING (+1 CASTING) widget should be created');
 
                 // Verify Title detail widget
